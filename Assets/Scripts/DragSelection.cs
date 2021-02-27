61a4ef1b1d44b5a2f43525fb09946071baad5be8
palapapa <palapapa93@gmail.com>
2021-02-27 19:04:26 +0800

    feat: drag selection
    Dragging the mouse on the screen will make a dotted selection box.

---
+++ Assets/Scripts/DragSelection.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class DragSelection : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    private bool isInitialClick = true;
+    private Vector3 startPosition;
+    private Vector3 endPosition;
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.widthMultiplier = 0.1f;
+        lineRenderer.numCapVertices = 16;
+        lineRenderer.numCornerVertices = 16;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if (isInitialClick)
+            {
+                startPosition = Input.mousePosition;
+                isInitialClick = false;
+            }
+            else
+            {
+                endPosition = Input.mousePosition;
+                List<Vector3> positions = new List<Vector3>
+                {
+                    startPosition,
+                    new Vector3(startPosition.x, endPosition.y, 0),
+                    endPosition,
+                    new Vector3(endPosition.x, startPosition.y, 0),
+                    startPosition
+                };
+                positions = Utilities.CalculateIntermidiatePoints(positions, 10);
+                for (int i = 0; i < positions.Count; i++)
+                {
+                    positions[i] = Utilities.ScreenToWorldPoint2D(positions[i]);
+                }
+                lineRenderer.positionCount = positions.Count;
+                lineRenderer.SetPositions(positions.ToArray());
+            }
+        }
+        else
+        {
+            isInitialClick = true;
+            lineRenderer.positionCount = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Utilities.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utilities
+{
+    /// <summary>
+    /// Convert a screen point to a world point with its z coordinate set to 0.
+    /// </summary>
+    /// <param name="v">The <see cref="Vector3"/> to convert.</param>
+    /// <returns>The converted screen point.</returns>
+    public static Vector3 ScreenToWorldPoint2D(Vector3 v)
+    {
+        v = Camera.main.ScreenToWorldPoint(v);
+        v = new Vector3(v.x, v.y, 0);
+        return v;
+    }
+
+    /// <summary>
+    /// Generate intermidiate points between <paramref name="start"/> and <paramref name="end"/>.
+    /// </summary>
+    /// <param name="start"></param>
+    /// <param name="end"></param>
+    /// <param name="count">The number of points to generate(including <paramref name="start"/> and <paramref name="end"/>).</param>
+    /// <returns></returns>
+    public static List<Vector3> CalculateIntermidiatePoints(Vector3 start, Vector3 end, int count)
+    {
+        if (count < 2)
+        {
+            throw new ArgumentException("Can't be less than 2", nameof(count));
+        }
+        List<Vector3> result = new List<Vector3>();
+        for (int i = 0; i < count; i++)
+        {
+            result.Add(Vector3.Lerp(start, end, 1.0f / count * i));
+        }
+        return result;
+    }
+
+    /// <summary>
+    /// Generate intermidiate points between each <paramref name="vector3s"/>.
+    /// </summary>
+    /// <param name="vector3s"></param>
+    /// <param name="count">The number of points to generate between each <paramref name="vector3s"/>(including each pair of <paramref name="vector3s"/>).</param>
+    /// <returns></returns>
+    public static List<Vector3> CalculateIntermidiatePoints(List<Vector3> vector3s, int count)
+    {
+        if (count < 2)
+        {
+            throw new ArgumentException("Can't be less than 2", nameof(count));
+        }
+        List<Vector3> result = new List<Vector3>();
+        for (int i = 0; i < vector3s.Count - 1; i++)
+        {
+            if (i != 0)
+            {
+                result.RemoveAt(result.Count - 1);
+            }
+            for (int j = 0; j < count; j++)
+            {
+                result.Add(Vector3.Lerp(vector3s[i], vector3s[i + 1], 1.0f / count * j));
+            }
+        }
+        result.RemoveAt(result.Count - 1);
+        result.Add(vector3s.Last());
+        return result;
+    }
+
+    public static T Last<T>(this IList<T> list)
+    {
+        return list[list.Count - 1];
+    }
+}
