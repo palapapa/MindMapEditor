e803242d825790a33d8aa4623a4f20661ef0cee5
palapapa <palapapa93@gmail.com>
2021-03-15 00:20:26 +0800

    refractor: migrate box drawing logic to its tool bar item
    Logic used to draw boxes are now moved to the box tool button script
    instead of having its own script.

--- Assets/Scripts/BoxTool.cs
+++ Assets/Scripts/BoxTool.cs
@@ -6,4 +6,58 @@ using UnityEngine.UI;
 
 public class BoxTool : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject box = default;
+    [SerializeField]
+    private GameObject mapObjects = default;
+    private LineRenderer lineRenderer;
+    private DragRectDrawer dragRectDrawer = new DragRectDrawer();
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        dragRectDrawer.DrawRect(lineRenderer, Input.GetKey(KeyCode.Mouse0) && UserData.Instance.ToolMode == ToolMode.Box && Background.Instance.IsMouseOnBackground);
+        if (Input.GetKeyUp(KeyCode.Mouse0) && UserData.Instance.ToolMode == ToolMode.Box && Background.Instance.IsMouseOnBackground)
+        {
+            GameObject newBox = Instantiate(box, mapObjects.transform);
+            RectTransform rectTransform = newBox.GetComponent<RectTransform>();
+            rectTransform.pivot = Vector2.zero;
+            Vector3 start = Utilities.ScreenToWorldPoint2D(dragRectDrawer.StartPosition);
+            Vector3 end = Utilities.ScreenToWorldPoint2D(dragRectDrawer.EndPosition);
+            Vector3 direction = end - start;
+            Vector3 originalStart = start;
+            Vector3 originalEnd = end;
+            if (direction.x < 0 && direction.y > 0) // quadrant 2
+            {
+                start = new Vector3(originalEnd.x, originalStart.y, 0);
+                end = new Vector3(originalStart.x, originalEnd.y, 0);
+            }
+            else if (direction.x < 0 && direction.y < 0) // quadrant 3
+            {
+                Utilities.Swap(ref start, ref end);
+            }
+            else if (direction.x > 0 && direction.y < 0) // quadrant 4
+            {
+                start = new Vector3(originalStart.x, originalEnd.y, 0);
+                end = new Vector3(originalEnd.x, originalStart.y, 0);
+            }
+            else
+            {
+                if (!(direction.x > 0 && direction.y > 0)) // on axis
+                {
+                    if (start.x > end.x || start.y > end.y)
+                    {
+                        Utilities.Swap(ref start, ref end);
+                    }
+                }
+            }
+            rectTransform.position = start;
+            rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, end.x - start.x);
+            rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, end.y - start.y);
+        }
+    }
 }
